2a9bdd91552f482982ab388c0ada2089616c38ca
emryscv <emrsycv@gmail.com>
2026-04-23 22:50:21 -0400

    feat/NavBar

---
+++ drone-club-one/app/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+export default function NavBar() {
+    return <header className='h-16 bg-black p-4 flex justify-between'>
+        <div className='flex'>
+            <Image
+                src="/droneClubLogo.png"
+                width="32"
+                height="32"
+                alt="Drone Club Logo"
+                style={{ filter: "invert(74%) sepia(68%) saturate(2283%) hue-rotate(1deg) brightness(104%) contrast(98%)" }}
+            />
+            <h1 className='text-2xl'>DRONE CLUB ONE</h1>
+        </div>
+
+        <nav>
+            <ul className='flex justify-between'>
+                <li className='pr-8'><span className='text-[#fcba03] pr-1 font-bold'>{'>'}</span>HOME</li>
+                <li className='pr-8'><span className='text-[#fcba03] pr-1 font-bold'>{'>'}</span>RACES</li>
+                <li className='pr-8'><span className='text-[#fcba03] pr-1 font-bold'>{'>'}</span>PILOTS</li>
+            </ul>
+        </nav>
+    </header>
+}
