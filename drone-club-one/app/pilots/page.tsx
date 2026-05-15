f2fce98b6ac0469e6e8ce29fb0312d6d81d3e9ae
emryscv <emrsycv@gmail.com>
2026-05-15 15:59:17 -0400

    feat/pilotsSearch

--- drone-club-one/app/pilots/page.tsx
+++ drone-club-one/app/pilots/page.tsx
@@ -1,16 +1,44 @@
+"use client";
 import TitleBorder from "../components/TitleBorder";
 import { getPilots } from "../data/queries/pilots";
 import PilotCard from "../components/PilotCard";
+import { Search } from "lucide-react";
+import { useEffect, useState } from "react";
+import { PilotTableType } from "../data/types";
+import { set } from "zod";
 
-export default async function Pilots() {
-    const pilotsList = await getPilots();
+export default function Pilots() {
+    const [pilotsList, setPilotsList] = useState<PilotTableType[]>([]);
+    const [filteredPilots, setFilteredPilots] = useState<PilotTableType[]>([]);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    useEffect(() => {
+        getPilots().then((data) => {
+            setPilotsList(data);
+        });
+    }, [])
+
+    useEffect(() => {
+        if (searchQuery !== "") {
+            setFilteredPilots(
+                pilotsList.filter((pilot) =>
+                    pilot.nickname.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()) ||
+                    pilot.firstname.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()) ||
+                    pilot.middlename?.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()) ||
+                    pilot.lastname.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase())
+                )
+            )
+        } else {
+            setFilteredPilots(pilotsList);
+        }
+    }, [searchQuery, pilotsList]);
 
     return <>
         <div className="mb-8">
             <TitleBorder>Pilots</TitleBorder>
             <p className="text-muted-foreground mt-4">All registered pilots</p>
 
-            {/* <div className="relative max-w-md mt-6">
+            <div className="relative max-w-md mt-6">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-muted-foreground" />
                 <input
                     type="text"
@@ -19,19 +47,19 @@ export default async function Pilots() {
                     onChange={(e) => setSearchQuery(e.target.value)}
                     className="w-full pl-10 pr-4 py-2 bg-input-background border border-border rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
                 />
-            </div> */}
+            </div>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pb-16">
-            {pilotsList.map((pilot) => (
+            {filteredPilots.map((pilot) => (
                 <PilotCard key={pilot.id} pilotData={pilot} />
             ))}
         </div>
 
-        {/* {filteredPilots.length === 0 && (
-        //     <div className="text-center py-12 text-muted-foreground">
-        //         No pilots found matching "{searchQuery}"
-        //     </div>
-        // )} */}
+        {filteredPilots.length === 0 && (
+            <div className="text-center py-12 text-muted-foreground">
+                No pilots found matching "{searchQuery}"
+            </div>
+        )}
     </>
 }
