564f19b41b50d901b1a758bb32cdda1bde514e09
emryscv <emrsycv@gmail.com>
2026-05-15 17:09:29 -0400

    feat/filterByRaceStatus

--- drone-club-one/app/races/page.tsx
+++ drone-club-one/app/races/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MapPin, Search } from "lucide-react";
+import { Calendar, MapPin, Search } from "lucide-react";
 import RaceCard from "../components/RaceCard";
 import TitleBorder from "../components/TitleBorder";
 import { getLocations, getRaces } from "../data/queries/races";
@@ -12,10 +12,11 @@ export default function Races() {
     const [racesData, setRacesData] = useState<RaceTableType[]>([]);
     const [filteredRaces, setFilteredRaces] = useState<RaceTableType[]>([]);
 
-    const [locations, setLocations] = useState<string[]>([]);
     const [searchQuery, setSearchQuery] = useState("");
-    const [selectedLocation, setSelectedLocation] = useState("");
+    const [locations, setLocations] = useState<string[]>([]);
 
+    const [selectedLocation, setSelectedLocation] = useState("");
+    const [selectedStatus, setSelectedStatus] = useState("");
 
     useEffect(() => {
         getRaces().then((data) => {
@@ -34,16 +35,22 @@ export default function Races() {
         if (searchQuery !== "") {
             filteredRaces = filteredRaces.filter((race) => race.title.toLowerCase().includes(searchQuery.toLowerCase()));
         }
+        if (selectedStatus !== "") {
+            filteredRaces = filteredRaces.filter((race) =>
+            (race.isupcoming && selectedStatus === "upcoming" ||
+                !race.isupcoming && selectedStatus === "Completed"));
+        }
+
         setFilteredRaces(filteredRaces);
-    }, [searchQuery, selectedLocation, racesData]);
+    }, [searchQuery, selectedLocation, selectedStatus, racesData]);
 
     return (
         <>
             <div className="mb-8">
                 <TitleBorder>Races</TitleBorder>
                 <p className="text-muted-foreground mt-4">All past and upcoming drone racing events</p>
-                <div className="flex mt-6 w-full gap-6">
-                    <div className="relative w-full max-w-md">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 w-full gap-6">
+                    <div className="relative">
                         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-accent" />
                         <input
                             type="text"
@@ -55,7 +62,7 @@ export default function Races() {
                             className="w-full pl-10 pr-4 py-2 border border-border rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
                         />
                     </div>
-                    <div className="relative w-full max-w-md">
+                    <div className="relative">
                         <MapPin className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-accent" />
                         <select
                             id="location"
@@ -68,6 +75,20 @@ export default function Races() {
                             {locations.map((location, i) => (
                                 <option key={i} value={location}>{location}</option>
                             ))}
+                        </select>
+                    </div>
+                    <div className="relative">
+                        <Calendar className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-accent" />
+                        <select
+                            id="status"
+                            name="status"
+                            value={selectedStatus}
+                            onChange={(e) => setSelectedStatus(e.target.value)}
+                            className="w-full pl-10 pr-4 py-2 border border-border rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
+                        >
+                            <option value="">Filter by Status</option>
+                            <option value="Completed">Completed</option>
+                            <option value="upcoming">Upcoming</option>
                         </select>
                     </div>
                 </div >
